afa9db7080b3aafd1f37a7ec7e7bdd9a5c4ef1ab
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2025-01-01 22:35:32 +0900

    環境をこのプロジェクト用に修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Lenis as ReactLenis } from "@studio-freight/react-lenis";
+import "./App.css";
+import Templates from "./components/templates";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ReactLenis root>
+      <Templates />
+    </ReactLenis>
+  );
 }
 
-export default App
+export default App;
