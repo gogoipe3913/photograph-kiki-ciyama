797274db83a62592e115c6d4a41c404b619bde07
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2025-02-17 14:48:23 +0900

    SPにステートメントを追加する

--- src/components/organisms/Top/index.tsx
+++ src/components/organisms/Top/index.tsx
@@ -97,6 +97,35 @@ const Top: React.FC<TopProps> = ({ onLoadData = () => {}, className = "" }) => {
           </FadeInContainer>
         ))}
       </ul>
+      <div className={style.Top__textSp}>
+        {/* <p className={style.Top__textProfileEn}>
+          Born in Shizuoka Prefecture in 1994, Taiki Kishiyama taught himself
+          graphic and web design during his university years, which led him to
+          discover photographic expression. After working for several companies,
+          he now operates as a freelance designer based in Tokyo and Kyoto.
+          While maintaining his practice in graphic design, web design, and web
+          development, he began creating personal work primarily using film
+          cameras in 2021. His artistic practice focuses on personal memories
+          and emotions that are often overlooked and forgotten in daily life,
+          capturing them from a perspective distinct from his commercial
+          photography work.
+        </p> */}
+        <p className={style.Top__textJa}>
+          かつて、大切なものを失う際の悲しさを、「その記憶自体を忘れること」で前に進んだ人は多いのではないだろうか、きっと僕もその一人だ。
+          <br />
+          破れるほど読んだ絵本や、睡眠時間を削って聞いた音楽、多くの時間を共に過ごした恋人のような、自分だけにとって、しかし非常に大切だと思えるものを。
+          <br />
+          <br />
+          僕たちはどうしたって忘れてしまうが、人は忘れる生き物であるという事実を、あまりにも当然のこととして受け入れ過ぎている。
+          <br />
+          大切に思っていたという気持ちの記録は、どこにも残らず過去の遺物となり、やがて遺物となったことすらも忘れ去られていく。忘れることが良いことだという論調を耳にする機会は、現代では、本当に枚挙にいとまがない。
+          <br />
+          <br />
+          本当に、忘れてしまってよいのだろうか。
+          <br />
+          あったはずのものがなくなったことや、居たはずの人がいないこと、好きだったものに何も感じなくなったこと。そんな、僕だけが大切に大切に想ってきたことに気づける写真を、残しておきたいと考えている。
+        </p>
+      </div>
     </div>
   );
 };
